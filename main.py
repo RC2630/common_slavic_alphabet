c700813cff60c4c3de74dd993c327af96da9fa13
Lucas Qin <lucasqin2002edu@gmail.com>
2024-04-16 19:48:31 -0700

    added contents of mapping files

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+from io import TextIOWrapper as File
+
+INPUT_FILE: str = "transliteration/input.txt"
+OUTPUT_FILE: str = "transliteration/output.txt"
+CYRILLIC_DEFAULT: str = "mappings/cyrillic_default.txt"
+CYRILLIC_SPECIFIC: str = "mappings/language_specific_cyrillic.txt"
+LATIN_SPECIFIC: str = "mappings/language_specific_latin.txt"
+UPPER_LOWER: str = "mappings/uppercase_lowercase.txt"
+
 if __name__ == "__main__":
     print(0)
